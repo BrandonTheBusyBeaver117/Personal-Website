ecf506cdc718b5e16d7fb21210dcfc79759ae56e
Brandon Nguyen <25nguyenb26@stu.smuhsd.org>
2025-02-24 09:46:07 -0800

    stripping the authy-firebase stuff

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,54 +1,20 @@
 'use client';
 
 import Image from 'next/image';
-import { useAuthState } from 'react-firebase-hooks/auth';
-import { auth } from '@/app/firebase/config';
 import { useRouter } from 'next/navigation';
 import { useEffect, useState } from 'react';
-import { signOut } from 'firebase/auth';
 import Loading from '../components/loading';
 
 const Dashboard: React.FC = () => {
   // Initializing router
   const router = useRouter();
 
-  // Initializing variables to track if user is authenticated
-  const [userAuth, authLoading] = useAuthState(auth);
-  const [resolved, setResolved] = useState<boolean>(false);
-
-  // Use Effect to check when authentication changes
-  useEffect(() => {
-    // If authentication hasn't finished, exit
-    if (authLoading) return;
-
-    // If the user is not authenticated, redirect them to home
-    // Otherwise, the authentication has been resolved, and we should show the user the dashboard
-    if (!userAuth) {
-      return router.push('/');
-    } else {
-      setResolved(true);
-    }
-  }, [userAuth, authLoading, router]);
-
-  // Too lazy to get the user's name or anything from the form
-  // I'll just read them back their email
-  const name = userAuth?.email;
-
   // If not resolved, show the loading page, otherwise show the dashboard
-  return !resolved ? (
-    <Loading />
-  ) : (
+  return (
     <>
-      <button
-        onClick={() => signOut(auth)}
-        className="absolute right-4 top-4 rounded-lg border-2 border-fuchsia-700 bg-fuchsia-500 px-4 py-2 text-white transition duration-300 ease-in-out hover:bg-pink-500"
-      >
-        Sign Out
-      </button>
-
       <div className="flex h-full w-1/2 flex-col items-center justify-evenly rounded-3xl border-4 border-sky-600 bg-sky-200">
         <div id="intro wrapper" className="flex grow-[3] basis-0 flex-col items-center text-2xl">
-          <h1 className="m-5">Hello {name}!</h1>
+          <h1 className="m-5">Hello!</h1>
           <h1>Welcome to your dashboard!</h1>
         </div>
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,47 +5,14 @@ import Link from 'next/link';
 
 import { useEffect, useState } from 'react';
 
-import { useAuthState } from 'react-firebase-hooks/auth';
 import { useRouter } from 'next/navigation';
-import { auth } from './firebase/config';
 import Loading from './components/loading';
 
 const Home: React.FC = () => {
   // Initializing router
   const router = useRouter();
 
-  // Initializing variables to track if user is authenticated
-  const [userAuth, authLoading] = useAuthState(auth);
-  const [resolved, setResolved] = useState<boolean>(false);
-
-  // Use Effect to check when authentication changes
-  useEffect(() => {
-    // If authentication hasn't finished, exit
-    if (authLoading) return;
-
-    // If the user is authenticated, redirect them to the dashboard
-    // Otherwise, the authentication has been resolved, and we should show the user the signup page
-    if (userAuth) {
-      return router.push('/dashboard');
-    } else {
-      setResolved(true);
-    }
-  }, [userAuth, authLoading, router]);
-
-  // Getting user from authstate
-  const [user] = useAuthState(auth);
-
-  // Logging in when there's no errors and a user exists
-  useEffect(() => {
-    if (user) {
-      return router.push('/dashboard');
-    }
-  }, [user, router]);
-
-  // If not resolved, show the loading page, otherwise show the page
-  return !resolved ? (
-    <Loading />
-  ) : (
+  return (
     <>
       <div className="flex min-h-72 w-full max-w-xl flex-col items-center justify-evenly rounded-lg bg-white px-8">
         <h1 className="text-center">
