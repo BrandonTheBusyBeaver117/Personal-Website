78e657e90c862c9453e5449cf44e9fb097df49ca
Brandon Nguyen <25nguyenb26@stu.smuhsd.org>
2025-03-06 12:31:54 -0800

    Makes loading unload
    if finished rendering - just get rid of loading component

--- src/app/components/loading.tsx
+++ src/app/components/loading.tsx
@@ -1,5 +1,11 @@
 // Creates a spinning loading screen
-const Loading: React.FC = () => {
+export type LoadingProps = {
+  isLoadingFinished: boolean;
+};
+
+const Loading: React.FC<LoadingProps> = ({ isLoadingFinished }) => {
+  if (isLoadingFinished) return <></>;
+
   return (
     <div className="flex h-4/5 w-2/5 flex-col items-center justify-center bg-gray-100">
       <h2 className="mb-6 text-sm font-bold text-gray-700">Loading...</h2>
